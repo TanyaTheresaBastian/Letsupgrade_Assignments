8ca4e9c5e189381dffa501ee35fd9d299a46ff84
Tanya Theresa Bastian AK <caeruleaa@gmail.com>
2022-10-06 18:49:51 +0530

    Update greatest.cpp
    C++ program to find which of the two numbers is greater.

--- 01Assignment/C++_Find_Greatest_of_two_numbers/greatest.cpp
+++ 01Assignment/C++_Find_Greatest_of_two_numbers/greatest.cpp
@@ -1,1 +1,19 @@
-
+#include <iostream>
+using namespace std;
+int main()
+{
+    float num1, num2;
+    cout<<"Enter First number:"; 
+    cin>>num1;
+    cout<<"Enter Second number:"; 
+    cin>>num2;
+    if(num1>num2)
+    {
+	cout<<"First number "<<num1<<" is the Greatest of the two.";
+    }
+    else
+    {
+	cout<<"Second number "<<num2<<" is the Greatest of the two";
+    }
+    return 0;
+  }
