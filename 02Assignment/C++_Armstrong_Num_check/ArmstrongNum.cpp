ee9bf66a6f8f7b1b5dec705f6ca48680c9ef432d
Tanya Theresa Bastian AK <caeruleaa@gmail.com>
2022-10-10 14:43:20 +0530

    Update ArmstrongNum.cpp

--- 02Assignment/C++_Armstrong_Num_check/ArmstrongNum.cpp
+++ 02Assignment/C++_Armstrong_Num_check/ArmstrongNum.cpp
@@ -1,1 +1,21 @@
 
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int num, temp, rem, sum = 0;
+    cout << "Enter number to be checked : ";
+    cin >> num;
+    temp = num;
+    while (temp != 0)
+    {
+        rem = temp % 10;
+        sum = sum + rem*rem*rem;
+        temp = temp / 10;
+    }
+    if (sum == num)
+        cout<< num << " is an Armstrong number.";
+    else
+        cout<< num << " is not an Armstrong number.";
+    return 0;
+  }
