27efb0bc82eefb80deec0b2d8495852c349ce170
Tanya Theresa Bastian AK <caeruleaa@gmail.com>
2022-10-10 14:51:36 +0530

    Create Factorial.cpp
    Find the Factorial of a given number.

---
+++ 02Assignment/C++_Factorial_of_num/Factorial.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int num,factorial=1;
+	cout<<"Enter Number To Find Its Factorial:  ";
+	cin>>num;
+	for (int a=1;a<=num;a++)
+    {
+		factorial=factorial*a;
+	  }
+	cout<<"Factorial of "<<num<<" is = "<<factorial;
+	return 0;
+}
